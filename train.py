e84d1152097b382223f7db78a0d03d27af1bc881
Naman Agrawal <na2603@columbia.edu>
2017-04-26 17:33:05 -0400

    Added cross validation to models for LIWC

--- train.py
+++ train.py
@@ -1,44 +1,53 @@
 import numpy as np
-import csv
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import csv
+
 from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+from sklearn import linear_model
+
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
 from sklearn.feature_selection import VarianceThreshold
-from sklearn import linear_model
+from sklearn.model_selection import cross_val_score
 
-def getData(fileType):
-    df = pd.read_csv(filepath_or_buffer='datasets/liwc_decade.csv', delimiter=",", header=None)
+def getData(modelType, filename):
+    df = pd.read_csv(filepath_or_buffer=filename, delimiter=",", header=None)
     data = df.values
 
-    if fileType == 'decade':
-        y_data = data[1:,2]
-    else:
-        y_data = data[1:,3]
+    if modelType == 'regression':
+        y_data = data[1:,2] # Song year
+    elif modelType == 'classifier':
+        y_data = data[1:,3] # Popularity label
 
     x_data = np.delete(data, [0,1,2,3], axis=1)
     x_data = np.delete(x_data, [0], axis=0)
+
+    #x_data = data[1:,[4]] #WC only
+
     return x_data, y_data
 
-def train(fileType):
-    X, y = getData(fileType)
+def model(modelType, filename):
+    X, y = getData(modelType, filename)
     #X = SelectKBest(f_classif, k=10).fit_transform(X, y)
     #X = VarianceThreshold(threshold=0.95).fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    if fileType == 'decade':
+    if modelType == 'regression':
         clf = linear_model.LinearRegression()
-    else:
-        clf = GaussianNB()
-        
-    clf.fit(X_train, y_train)
-    train_accuracy = clf.score(X_train, y_train)
-    test_accuracy = clf.score(X_test, y_test)
-    print("Training accuracy: {}".format(train_accuracy))
-    print("Testing accuracy: {}".format(test_accuracy))
+    elif modelType == 'classifier':
+        #clf = GaussianNB()
+        clf = SVC(kernel='linear')
 
-train('decade')
+    scores = cross_val_score(clf, X, y, cv=10)
+    return scores.mean(), scores.std() * 2
+
+if __name__ == "__main__":
+    #Regression
+    #mean, std = model('regression', 'datasets/liwc_decade.csv')
+
+    #Classification
+    mean, std = model('classifier', 'datasets/liwc_all.csv')
+    #mean, std = model('regression', 'datasets/liwc_rihanna.csv')
+    #mean, std = model('regression', 'datasets/liwc_drake.csv')
+
+    print("Score: %0.2f (+/- %0.2f)" % (mean, std * 2))
