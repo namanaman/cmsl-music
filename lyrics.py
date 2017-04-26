d3eba5a696310cd59be7429fb7b104c42d1de036
Naman Agrawal <na2603@columbia.edu>
2017-04-25 20:16:04 -0400

    Added data collection scripts and final datasets

--- lyrics.py
+++ lyrics.py
@@ -5,25 +5,13 @@ import csv
 import requests
 import re
 from bs4 import BeautifulSoup
-
-def readCSV():
-    filename = 'BillboardHot100Data.csv'
-    with open(filename, 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-        data = list(reader)
-
-    for i in range(len(data)):
-        split_artists = re.split(' x | Featuring ', data[i][1])
-        data[i][1] = split_artists[0]
-
-    return data
+import pickle
 
 def getLyrics(song_api_path, headers, base_url):
     song_url = base_url + song_api_path
-    response = requests.get(song_url, headers=headers)
-    json = response.json()
+    r = requests.get(song_url, headers=headers).json()
 
-    path = json["response"]["song"]["path"]
+    path = r["response"]["song"]["path"]
     page_url = "http://genius.com" + path
     page = requests.get(page_url)
 
@@ -32,24 +20,92 @@ def getLyrics(song_api_path, headers, base_url):
     lyrics = html.find("lyrics").get_text()
     return lyrics
 
-if __name__ == "__main__":
+def saveData(csvData, filename):
     base_url = "http://api.genius.com"
     headers = {'Authorization': 'Bearer TOKEN'} #TOKEN replaced by actual
     search_url = base_url + "/search"
-    csvData = readCSV()
-    songs = len(csvData)
-    for i in range(songs):
-        data = {'q': csvData[i][0]}
-        response = requests.get(search_url, params=data, headers=headers)
-        json = response.json()
+
+    lyricData = []
+    for i in range(len(csvData)):
+        data = {'q': csvData[i][1]}
+        r = requests.get(search_url, params=data, headers=headers).json()
 
         song_info = None
-        for hit in json["response"]["hits"]:
-            if hit["result"]["primary_artist"]["name"].lower() == csvData[i][1].lower():
+        for hit in r["response"]["hits"]:
+            if hit["result"]["primary_artist"]["name"].lower() == csvData[i][0].lower():
                 song_info = hit
                 break
 
         if song_info:
-            count = count + 1
             song_api_path = song_info["result"]["api_path"]
-            getLyrics(song_api_path, headers, base_url)
+            lyrics = getLyrics(song_api_path, headers, base_url)
+            lyricData.append([csvData[i][1], csvData[i][0], lyrics, csvData[i][2]])
+
+    print(len(lyricData))
+    with open(filename, 'w', newline='') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(lyricData)
+
+def getArtistData():
+    with open('Popular-v-Unpopular.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = list(reader)
+
+    drakeData = data[:113]
+    rihannaData = data[113:197]
+    coldplayData = data[197:]
+
+    saveData(drakeData, 'drakeData.csv')
+    saveData(rihannaData, 'rihannaData.csv')
+    saveData(coldplayData, 'coldplayData.csv')
+
+def getSongData():
+    with open('updated_Hot100Data.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = list(reader)
+
+    for i in range(len(data)):
+        split_artists = re.split(' x | Featuring ', data[i][1])
+        data[i][1] = split_artists[0]
+
+    saveData(data, 'popularLyrics.csv')
+
+def combineCSV():
+    with open('popularExtraSongs.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = list(reader)
+
+    with open(r'popularLyrics.csv', 'a') as f:
+        writer = csv.writer(f)
+        for song in data:
+            writer.writerow([song[0], song[1], song[2], lyrics])
+
+def cleanData():
+    with open('final/drakeData.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = list(reader)
+
+    for song in data:
+        lyrics = song[3]
+        lyrics2 = lyrics.replace('\r', ' ').replace('\n', ' ')
+        lyrics3 = ' '.join(lyrics2.split())
+        song[3] = re.sub("[\(\[].*?[\)\]]", "", lyrics3)
+
+    with open('clean_drakeData.csv', 'w', newline='') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(data)
+
+if __name__ == "__main__":
+    with open('coldplayData.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = list(reader)
+
+    for song in data:
+        lyrics = song[2]
+        lyrics2 = lyrics.replace('\r', ' ').replace('\n', ' ')
+        lyrics3 = ' '.join(lyrics2.split())
+        song[2] = re.sub("[\(\[].*?[\)\]]", "", lyrics3)
+
+    with open('final_coldplayData.csv', 'w', newline='') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(data)
---
+++ unpopularSongs.py
@@ -0,0 +1,62 @@
+import csv
+import requests
+import base64
+import pickle
+
+def readCSV():
+    filename = 'billboardSongSpotifyID.csv'
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    data = [val for sublist in data for val in sublist] #Flatten list
+    return data
+
+def getAlbumInfo(songID, token):
+    song_url = 'https://api.spotify.com/v1/tracks/' + songID
+    r = requests.get(song_url, headers={'Authorization': token}).json()
+
+    albumType = r['album']['album_type']
+    if albumType == 'album':
+        album_url = 'https://api.spotify.com/v1/albums/' + r['album']['id']
+        album_response = requests.get(album_url, headers={'Authorization': token})
+        album_json = album_response.json()
+        album_popularity = album_json['popularity']
+
+        trackIDs = []
+        for track in album_json['tracks']['items']:
+            trackIDs.append(track['id'])
+
+        stringIDs = ','.join(trackIDs)
+        track_url = 'https://api.spotify.com/v1/tracks/'
+        track_response = requests.get(track_url, params={'ids': stringIDs}, headers={'Authorization': token}).json()
+        for track in track_response['tracks']:
+            trackName = track['name']
+            popularity = track['popularity']
+            if popularity + 10 < album_popularity:
+                break
+
+        return r['name'], trackName, r['artists'][0]['name']
+
+def getUnpopular():
+    token = 'Bearer TOKEN'
+    songIDs = readCSV()
+
+    pkl_file = open('Unpopular.pkl', 'rb')
+    unpopular_songs = pickle.load(pkl_file)
+
+    for song in songIDs:
+        pair = getAlbumInfo(song, token)
+        if pair:
+            original, unpopular, artist = pair
+            unpopular_songs[original] = (unpopular, artist)
+
+    return unpopular_songs
+
+if __name__ == "__main__":
+    #unpop = getUnpopular()
+    #with open('Unpopular.pkl', 'wb') as f:
+        #pickle.dump(unpop, f, pickle.HIGHEST_PROTOCOL)
+
+    pkl_file = open('Unpopular.pkl', 'rb')
+    unpopular_songs = pickle.load(pkl_file)
+    print(len(unpopular_songs.keys()))
